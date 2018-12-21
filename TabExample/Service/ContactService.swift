c0fd9220707a0ce671cd999f7c91affccf2ad764
Daniel <d_sanchez@ar3d.net>
2018-12-20 23:33:59 -0600

    Moved controllers. Added real data to ContactServices.

--- TabExample/Service/ContactService.swift
+++ TabExample/Service/ContactService.swift
@@ -12,14 +12,23 @@ class ContactService: NSObject {
     
     class func fetchContacts(_ success: @escaping (Array<Contact>) -> Void) {
         var contacts: Array<Contact> = []
-        for i in 0...9 {
-            contacts.append(Contact(name: "Contact \(i)", lastName: "Last Name X\(i)",age: 20 + i, phoneNumber: "3333333333", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
-        }
+        
+        contacts.append(Contact(name: "Daniel", lastName: "Sánchez",age: 23, phoneNumber: "3351413316", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Jessica", lastName: "Arvizu",age: 22, phoneNumber: "5514758910", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Rocío", lastName: "Ramírez",age: 25, phoneNumber: "9615114120", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Edgar", lastName: "Saucedo",age: 55, phoneNumber: "7541225560", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Leopoldo", lastName: "Gonzalez",age: 15, phoneNumber: "44856633", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Ana", lastName: "Torres",age: 25, phoneNumber: "330319852", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Andrea", lastName: "Davila",age: 40, phoneNumber: "78584525", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Alejandro", lastName: "Niebla",age: 20, phoneNumber: "45214355", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Jesús", lastName: "Fuentes",age: 29, phoneNumber: "66981255", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        contacts.append(Contact(name: "Arturo", lastName: "Placencia",age: 59, phoneNumber: "10101010", profileImage: "https://mcmp.com.au/wp-content/uploads/2017/08/profile-placeholder.jpg"))
+        
         ContactStore.shared.contacts = contacts
         success(contacts)
-//        let popTime = DispatchTime.now() + Double(Int64( Double(NSEC_PER_SEC) * 3 )) / Double(NSEC_PER_SEC)
-//        DispatchQueue.main.asyncAfter(deadline: popTime) {
-//
-//        }
+        //        let popTime = DispatchTime.now() + Double(Int64( Double(NSEC_PER_SEC) * 3 )) / Double(NSEC_PER_SEC)
+        //        DispatchQueue.main.asyncAfter(deadline: popTime) {
+        //
+        //        }
     }
 }
